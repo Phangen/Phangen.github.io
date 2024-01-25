ca1f85236c09af51a23d02a8238fb1fd57d1dcf5
phangen <ph.hangen@gmail.com>
2024-01-25 16:34:57 -0500

    Footer Custom Element

---
+++ js/myFooter.js
@@ -0,0 +1,30 @@
+class myFooter extends HTMLElement {
+    constructor() {
+      super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+          
+        <footer class="bg-white py-4 mt-auto">
+            <div class="container px-5">
+                <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+                    <!-- <div class="col-auto"><div class="small m-0">Copyright &copy; Your Website 2023</div></div> -->
+                    <div class="col-auto"><div class="small m-0"></div></div>
+                    <div class="col-auto">
+                        <!-- <a class="small" href="#!">Privacy</a>
+                        <span class="mx-1">&middot;</span>
+                        <a class="small" href="#!">Terms</a>
+                        <span class="mx-1">&middot;</span>-->
+                        <a class="small" href="contact.html">Contact</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+        `;
+      }
+
+  }
+  
+  customElements.define('footer-component', myFooter);
