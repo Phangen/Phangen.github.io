39348a87d61d04ac4d192ba0ada1197b1d0b2480
phangen <ph.hangen@gmail.com>
2024-01-27 18:16:23 -0500

    Implementation of Project Body Custom Elements

--- js/projectHelpers.js
+++ js/projectHelpers.js
@@ -3,7 +3,7 @@ class projectHeader extends HTMLElement {
       super();
     }
 
-    attributeChangedCallback(name, oldValue, newValue) { // (4)
+    attributeChangedCallback(name, oldValue, newValue) {
       this.render();
     }
 
@@ -14,29 +14,28 @@ class projectHeader extends HTMLElement {
       }
     }
 
-    static get observedAttributes() { // (3)
+    static get observedAttributes() {
       return ['projectTitle', 'projectSummary', 'downloadLink', 'videoLink', 'videoLink'];
     }
 
     render() {
         let projectTitle = 
             '<h1 class="display-3 fw-bolder"><span class="text-gradient d-inline">' + this.getAttribute('projectTitle') + '</span></h1>';
-        console.log(projectTitle);
+        
         let projectSummary = 
             '<div class="fs-3 fw-light text-muted mb-5">' + this.getAttribute('projectSummary') + '</div>';
-        console.log(projectSummary);
+        
         let downloadLink = 
             '<div class="card shadow border-0 rounded-4 mb-5 px-5 py-3 me-sm-3 fs-6 fw-bolder">Download Unavailable</div>';
         if (this.getAttribute('downloadLink') != null){
           downloadLink =
               '<a class="btn btn-primary btn-lg px-5 py-3 me-sm-3 fs-6 fw-bolder" href="'+ this.getAttribute('downloadLink') +'">Download</a>';
         }
-        console.log(downloadLink);
+        
         let video = 
             '<iframe width="600" height="315" src="' + this.getAttribute('videoLink') + '" title="' + this.getAttribute('videoLink') + '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
         
-        console.log(video);
-
+        
         this.innerHTML = `
           
         <div class="container px-5 pb-5">
@@ -73,4 +72,130 @@ class projectHeader extends HTMLElement {
 
   }
   
+  const bodyTmp = document.createElement('template');
+  bodyTmp.innerHTML = `
+    <link href="css/styles.css" rel="stylesheet"/>
+    <div class="card shadow border-0 rounded-4 mb-5">
+      <div class="card-body p-5">
+        <div class="row align-items-center gx-5">
+          <div>
+            <slot></slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+  
+  class projectBodyMain extends HTMLElement {
+    
+    constructor() {
+      super();
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+      this.render();
+    }
+
+    connectedCallback() {
+      if (!this.rendered) {
+        this.render();
+        this.rendered = true;
+      }
+    }
+
+    static get observedAttributes() {
+      return ['bodyText', 'bodyTitle'];
+    }
+
+    render() {
+      const shadowRoot = this.attachShadow({ mode: 'closed' });
+
+      shadowRoot.appendChild(bodyTmp.content.cloneNode(true));
+    }
+  
+  }
+  
+
+  const bodyBrightTmp = document.createElement('template');
+  bodyBrightTmp.innerHTML = `
+  <link href="css/styles.css" rel="stylesheet"/>
+  <div class="col text-center text-lg-start mb-5 mb-lg-0">
+    <div class="bg-white p-4 rounded-4">
+      <slot></slot>
+    </div>
+  </div>
+  `;
+  
+  class projectBodyBright extends HTMLElement {
+    
+    constructor() {
+      super();
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+      this.render();
+    }
+
+    connectedCallback() {
+      if (!this.rendered) {
+        this.render();
+        this.rendered = true;
+      }
+    }
+
+    static get observedAttributes() {
+      return ['bodyText', 'bodyTitle'];
+    }
+
+    render() {
+      const shadowRoot = this.attachShadow({ mode: 'closed' });
+
+      shadowRoot.appendChild(bodyBrightTmp.content.cloneNode(true));
+    }
+  
+  }
+
+  const bodyDarkTmp = document.createElement('template');
+  bodyDarkTmp.innerHTML = `
+    <link href="css/styles.css" rel="stylesheet"/>
+    <div class="col text-center text-lg-start mb-5 mb-lg-0">
+      <div class="bg-light p-4 rounded-4">
+        <slot></slot>
+      </div>
+    </div>
+  `;
+  
+  class projectBodyDark extends HTMLElement {
+    
+    constructor() {
+      super();
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+      this.render();
+    }
+
+    connectedCallback() {
+      if (!this.rendered) {
+        this.render();
+        this.rendered = true;
+      }
+    }
+
+    static get observedAttributes() {
+      return ['bodyText', 'bodyTitle'];
+    }
+
+    render() {
+      const shadowRoot = this.attachShadow({ mode: 'closed' });
+
+      shadowRoot.appendChild(bodyDarkTmp.content.cloneNode(true));
+    }
+  
+  }
+
   customElements.define('project-header-component', projectHeader);
+
+  customElements.define('project-body-main', projectBodyMain);
+  customElements.define('project-body-bright', projectBodyBright);
+  customElements.define('project-body-dark', projectBodyDark);
