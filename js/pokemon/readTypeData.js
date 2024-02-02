0b38457f0d0d9f9aaf992035833eeeb7f7defea1
phangen <ph.hangen@gmail.com>
2024-02-01 21:30:32 -0500

    Calculating type combination effectiveness

--- js/pokemon/readTypeData.js
+++ js/pokemon/readTypeData.js
@@ -1,16 +1,57 @@
 import { getJson } from "../JSONreader.js";
+import ArrayLogic from "../ArrayLogicModule.js";
 
 class PokemonType {
 
-    constructor(){
+    constructor(typeJson, typeTwo) {
+        if (typeTwo == undefined)
+        {
+            for (const jsonName in typeJson){
+                this.typeName = jsonName;
+            }
+            this.twiceSuperEff = [];
+            this.twiceNotVeryEff  = [];
+            this.superEff = typeJson[this.typeName].superEff;
+            this.notVeryEff = typeJson[this.typeName].notVeryEff;
+            this.immune = typeJson[this.typeName].immune;
+        }
+        else 
+        {
+            let typeOne = typeJson;
+
+            this.typeName = typeOne.typeName + "." + typeTwo.typeName;
+            this.twiceSuperEff = ArrayLogic.intersection(typeOne.superEff, typeTwo.superEff);
+            this.superEff = ArrayLogic.setDifference(ArrayLogic.difference(typeOne.superEff, typeTwo.notVeryEff), (ArrayLogic.difference(typeTwo.superEff, typeOne.notVeryEff)));
+            this.notVeryEff = ArrayLogic.setDifference(ArrayLogic.difference(typeOne.notVeryEff, typeTwo.superEff), (ArrayLogic.difference(typeTwo.notVeryEff, typeOne.superEff)));
+            this.twiceNotVeryEff  = ArrayLogic.intersection(typeOne.notVeryEff, typeTwo.notVeryEff);
+            this.immune = ArrayLogic.union(typeOne.immune, typeTwo.immune);
+        }        
 
     }
+
+
+
 }
 
 getJson('assets/pokemon/typeData.json').then((json) => {
     console.log(json);
     console.log(json.types)
-    for (const pokeType in json.types) {
-        console.log(pokeType);
-    }
+    let typeArray = [];
+    json.types.forEach((element) => {
+        console.log(element);
+        let pokeType = new PokemonType(element);
+        console.log(pokeType.typeName);
+        console.log(pokeType.superEff);
+        console.log(pokeType.notVeryEff);
+        console.log(pokeType.immune);
+        typeArray.push(pokeType);
+    });
+    let testDualType = new PokemonType(typeArray[4], typeArray[2]);
+    console.log(testDualType.typeName);
+    console.log(testDualType.twiceSuperEff);
+    console.log(testDualType.superEff);
+    console.log(testDualType.notVeryEff);
+    console.log(testDualType.twiceNotVeryEff);
+    console.log(testDualType.immune);
+
 });
