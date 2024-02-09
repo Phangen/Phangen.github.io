6a2c7501045690c79a7641b5bb708c23ae9bc29d
phangen <ph.hangen@gmail.com>
2024-02-08 19:34:30 -0500

    adding css to implement mobile support

--- js/HTMLElement/myPokemonButton.js
+++ js/HTMLElement/myPokemonButton.js
@@ -1,44 +1,75 @@
 const buttTmp = document.createElement('template');
 buttTmp.innerHTML = `
-  <style>
-  element.style{
-      --type-color: white
-  }
-  .pButton {
-      border: 4px solid var(--type-color);
-      background-color: transparent;
-      color: black;
-      padding: 15px 32px;
-      text-align: center;
-      text-decoration: none;
-      display: inline-block;
-      font-size: 16px;
-      transition-duration: 0.4s;
-      opacity: 0.75;
-      border-radius: 12px;
-      width: 125px;
-      cursor: pointer;
-  }
-  .pButton[aria-pressed="true"] {
-      border: none;
-      background-color: var(--type-color);
-      padding: 19px 32px;
-      box-shadow:
-            inset 0 0 2px 0 rgba(255,255,255,.4),
-            inset 0 0 3px 0 rgba(0,0,0,.4),
-            inset 0 0 3px 5px rgba(0,0,0,.05);
-      color: white;
-  }
-  .pButton:hover {
-      opacity: 1;
-  }
-  .pButton:disabled {
-      cursor: not-allowed;
-      background-color: #37373780;
-  }
-  </style>
+<style>
+element.style{
+    --type-color: white
+}
+/* For mobile phones: */
+    [class*="pButton"] {
+        border: 4px solid var(--type-color);
+        background-color: transparent;
+        color: black;
+        padding: 10px 16px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        transition-duration: 0.4s;
+        opacity: 0.75;
+        border-radius: 12px;
+        width: 100%;
+        cursor: pointer;
+    }
+    .pButton[aria-pressed="true"] {
+        border: none;
+        background-color: var(--type-color);
+        padding: 14px 16px;
+        box-shadow:
+            inset 0 0 2px 0 #ffffff66,
+            inset 0 0 3px 0 #00000066,
+            inset 0 0 3px 5px #0000000d;
+        color: white;
+    }
+
+@media only screen and (min-width: 768px) {
+    /* For desktop: */
+    .pButton {
+        border: 4px solid var(--type-color);
+        background-color: transparent;
+        color: black;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        transition-duration: 0.4s;
+        opacity: 0.75;
+        border-radius: 12px;
+        width: 125px;
+        cursor: pointer;
+    }
+    .pButton[aria-pressed="true"] {
+        border: none;
+        background-color: var(--type-color);
+        padding: 19px 32px;
+        box-shadow:
+            inset 0 0 2px 0 #ffffff66,
+            inset 0 0 3px 0 #00000066,
+            inset 0 0 3px 5px #0000000d;
+        color: white;
+    }
+}
+
+.pButton:hover {
+    opacity: 1;
+}
+.pButton:disabled {
+    cursor: not-allowed;
+    background-color: #37373780;
+}
+</style>
   <button class="pButton" type="button" aria-pressed="false">  
-  <slot></slot>
+    <slot></slot>
   </button> 
   `;
 
@@ -80,7 +111,22 @@ typeCardTmp.innerHTML = `
       --type-color: white;
       --type-darker: black;
   }
-  .tCard {
+  /* For mobile phones: */
+    [class*="tCard"] {
+      border: 5px solid var(--type-color);
+      background-color: var(--type-darker);
+      color: white;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      font-size: 16px;
+      border-radius: 3px;
+      width: 100%;
+      box-sizing: border-box;
+    }
+
+  @media only screen and (min-width: 768px) {
+    .tCard {
       border: 5px solid var(--type-color);
       background-color: var(--type-darker);
       color: white;
@@ -90,7 +136,10 @@ typeCardTmp.innerHTML = `
       font-size: 16px;
       border-radius: 3px;
       width: 125px;
+    }
   }
+  
+  
   </style>
   <div class="tCard">  
       <slot></slot>
